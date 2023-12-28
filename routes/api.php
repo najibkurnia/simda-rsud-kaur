479e4e266f6f65895a952d8c24fed0d8e5a1c550
Neville Jeremy <124435554+nepile@users.noreply.github.com>
2023-12-29 02:02:01 +0700

    fix: fixed route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthenticationController;
+use App\Http\Controllers\Api\PermintaanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthenticationController::class, 'login']);
 Route::post('/register', [AuthenticationController::class, 'register']);
 Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/handle-permintaan', [PermintaanController::class, 'handlePermintaan']);
+});
